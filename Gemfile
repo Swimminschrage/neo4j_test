ff1af7d5e94a06322016235ea620488efe19810f
Swimminschrage <Swimminschrage@gmail.com>
2015-04-14 20:23:59 -0500

    Adding some initial tests for the Neo4jTest::Installer class.

--- Gemfile
+++ Gemfile
@@ -11,7 +11,8 @@ gem 'zip'
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
 group :development do
-  gem 'rspec', '~> 2.8.0'
+  gem 'rspec', '~> 3.2.0'
+  gem 'rspec-mocks'
   gem 'rdoc', '~> 3.12'
   gem 'bundler', '~> 1.0'
   gem 'jeweler', '~> 2.0.1'
--- spec/neo4j_test_spec.rb
+++ spec/neo4j_test_spec.rb
@@ -1,7 +1,67 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'spec_helper'
+require 'fileutils'
 
-describe "Neo4jTest" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe 'Neo4jTest' do
+  let(:test_directory) { 'tmp/test' }
+
+  describe '::Server' do
+
+  end
+
+  describe '::Installer' do
+    before(:each) do
+      FileUtils.mkdir_p test_directory
+    end
+
+    after(:each) do
+      FileUtils.remove_dir(test_directory) if File.exist? test_directory
+    end
+
+    describe '#file_name' do
+      it 'return the correct filename for windows' do
+        allow(OS::Underlying).to receive(:windows?) { true }
+
+        expect(Neo4jTest::Installer.file_name).to eq 'neo4j.zip'
+      end
+
+      it 'returns the correct filename for unix' do
+        allow(OS::Underlying).to receive(:windows?) { false }
+
+        expect(Neo4jTest::Installer.file_name).to eq 'neo4j-unix.tar.gz'
+      end
+    end
+
+    describe '#download_neo4j' do
+
+      it 'downloads the correct file' do
+        allow(Neo4jTest::Installer).to receive(:request_url).and_return('Test data')
+        allow(Neo4jTest::Installer).to receive(:download_to).and_return(File.join(test_directory, 'test.txt'))
+
+        Neo4jTest::Installer.download_neo4j 'community-2.2.0'
+        expect(File.exist?('tmp/test')).to be true
+      end
+    end
+
+    describe '#download_neo4j_unless_exists' do
+      it 'downloads the correct file the first time' do
+        allow(Neo4jTest::Installer).to receive(:request_url).and_return('Test data')
+        allow(Neo4jTest::Installer).to receive(:download_to).and_return(File.join(test_directory, 'test.txt'))
+
+        Neo4jTest::Installer.download_neo4j 'community-2.2.0'
+        expect(File.exist?('tmp/test')).to be true
+      end
+
+      it 'doesnt download on subsequent tries' do
+        allow(Neo4jTest::Installer).to receive(:request_url).and_return('Test data')
+        allow(Neo4jTest::Installer).to receive(:download_to).and_return(File.join(test_directory, 'test.txt'))
+
+        Neo4jTest::Installer.download_neo4j_unless_exists 'community-2.2.0'
+        expect(File.exist?('tmp/test')).to be true
+
+        expect(Neo4jTest::Installer).to_not receive(:download_neo4j)
+        Neo4jTest::Installer.download_neo4j_unless_exists 'community-2.2.0'
+        expect(File.exist?('tmp/test')).to be true
+      end
+    end
   end
 end
